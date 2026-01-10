e3b77335a3eb1d80abf4f46855580088734729fb
maelho <51026438+maelho@users.noreply.github.com>
2026-01-10 19:12:54 -0400

    add TanStack Query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
-import './layout.css'
+  import './layout.css'
   import favicon from '$lib/assets/favicon.svg'
+  import { browser } from '$app/environment'
+
+  import { QueryClientProvider, QueryClient } from '@tanstack/svelte-query'
+  import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools'
 
   const { children } = $props()
+
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        enabled: browser
+      }
+    }
+  })
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<QueryClientProvider client={queryClient}>
+  {@render children()}
+  <SvelteQueryDevtools />
+</QueryClientProvider>
