0cb4114b04c2fb8fe53992840dd622dd9f84b70e
maelho <51026438+maelho@users.noreply.github.com>
2026-02-01 19:14:04 -0400

    add infinite scroll

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
-  import GamesGrid from '$lib/components/games-grid.svelte'
-  import { gameQueryOptions } from '$lib/query-options'
-  import { createInfiniteQuery } from '@tanstack/svelte-query'
-
-  const gameInfiniteQuery = createInfiniteQuery(gameQueryOptions)
+  import GamesInfiniteScroll from '$lib/components/games-infinite-scroll.svelte'
 </script>
 
 <div class="container mx-auto px-6 py-6">
-  {#if gameInfiniteQuery.isFetching}
-    <h1>loading...</h1>
-  {:else if gameInfiniteQuery.isSuccess}
-    <GamesGrid data={gameInfiniteQuery.data.pages} />
-  {/if}
+  <GamesInfiniteScroll />
 </div>
